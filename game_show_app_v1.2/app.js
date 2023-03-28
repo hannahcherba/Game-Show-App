dcd80b131b8192fb5a714e6939fbc5edb329b290
Hannah Cherba <hannahpond@yahoo.com>
2023-03-28 15:31:08 -0700

    Created first half of javascript

--- game_show_app_v1.2/app.js
+++ game_show_app_v1.2/app.js
@@ -18,26 +18,26 @@ let startButton = document.getElementsByClassName('btn__reset');
 /*Create a phrases array that contains at least 5 different phrases as strings.
 Make sure that each phrase contains only letters and spaces, so players won’t need to guess punctuation or special characters.*/
 
-const phrases= {
-    0: "total depravity",
-    1: "unconditional election",
-    2: "limited atonement",
-    3: "irresistible grace",
-    4: "perseverance of the saints",
-    5: "sola scriptura",
-    6: "solus christus",
-    7: "sola gratia",
-    8: "sola fide",
-    9: "soli deo gloria",
-};
+const phrases= [
+    "total depravity",
+    "unconditional election",
+    "limited atonement",
+    "irresistible grace",
+     "perseverance of the saints",
+    "sola scriptura",
+     "solus christus",
+    "sola gratia",
+     "sola fide",
+     "soli deo gloria",
+    ];
 
 //Attach an event listener to the “Start Game” button to hide the start screen overlay.
 
-startButton(addEventListener)('button', e => {
-
-
-
-})
+startButton[0].addEventListener('click', e => {
+    if (startButton) {
+        document.getElementById('overlay').style.display = 'none';
+    }
+});
 
 /*Create a getRandomPhraseAsArray function.
 This function should randomly choose a phrase from the phrases array and split that phrase into a new array of characters. The function should 
@@ -46,13 +46,13 @@ then return the new character array. Keep in mind that you’ll need to write th
  the function so that it takes an array as a parameter. */
 
  function getRandomPhraseAsArray(arr) {
-    //randomly choose phrases from array
-    //split phrase into new array of characters
-    //function should return array of characters from an given array of strings
 
+    let phrase = arr[Math.floor(Math.random() * arr.length)];
+    let chars = phrase.split("");
+    return chars;
  };
 
- getRandomPhraseAsArray(phrases);
+//  getRandomPhraseAsArray(phrases);
 
  /* Set the game display.
  =========================
@@ -63,8 +63,27 @@ a letter and not a space, the function should add the class “letter” to the 
 You’ll need to write the addPhraseToDisplay function so that it can take any array of letters 
 and add it to the display. To do that, the function will need to take an array as a parameter */
 
+const phraseArray = getRandomPhraseAsArray(phrases);
+
 const addPhraseToDisplay = (arr) => {
 
+    let ul = document.getElementById('phrase');
+ 
+    phraseArray.forEach((item)=>{
+
+        let li = document.createElement("li");
+        li.innerText = item;
+        ul.appendChild(li);
+
+        const result = phraseArray.filter( phraseArray => phraseArray.length === /^[A-Za-z]+$/);
+
+        if (result) {
+
+            li.className = "letter";
+        }
+
+    })
+    
     /*for loop to loop through an array of characters
     * for each character in the array in the loop:
      -create list item (li) 
@@ -72,13 +91,26 @@ const addPhraseToDisplay = (arr) => {
      -append list item to #phrase ul in html
      -(if the character is a letter, not a space, function
         should add class "letter" to the li item) */
+   
+
 };
 
 //To use the function, you’ll get the value returned by the getRandomPhraseAsArray, save it to a variable, and pass it to addPhraseToDisplay as an argument:
 
-const phraseArray = getRandomPhraseAsArray(phrases);
 
-addPhrasetoDisplay(phraseArray); 
+addPhraseToDisplay(phraseArray); 
+
+
+
+/*Add an event listener to the keyboard.
+========================================
+Use event delegation to listen only to button events from the keyboard. When a player chooses a letter, 
+add the “chosen” class to that button so the same letter can’t be chosen twice. Note that button elements
+ have an attribute you can set called “disabled” that when set to true will not respond to user clicks. 
+ See the MDN documentation for more details.
+Pass the button to the checkLetter function, and store the letter returned inside of a variable called 
+letterFound. At this point, you can open the index.html file, click any of the letters on the keyboard, 
+and start to see the letters appear in the phrase. */
 
 /* Create a checkLetter function inside the keyboard Event Listener.
 ==================================
@@ -91,16 +123,6 @@ elements with a class of “letter” (remember that we added the letter class t
 If there’s a match, the function should add the “show” class to the list item containing that letter,
  store the matching letter inside of a variable, and return that letter.
 If a match wasn’t found, the function should return null. */
-
-/*Add an event listener to the keyboard.
-========================================
-Use event delegation to listen only to button events from the keyboard. When a player chooses a letter, 
-add the “chosen” class to that button so the same letter can’t be chosen twice. Note that button elements
- have an attribute you can set called “disabled” that when set to true will not respond to user clicks. 
- See the MDN documentation for more details.
-Pass the button to the checkLetter function, and store the letter returned inside of a variable called 
-letterFound. At this point, you can open the index.html file, click any of the letters on the keyboard, 
-and start to see the letters appear in the phrase. */
 
 /*Count the missed guesses in the game.
 ========================================
@@ -119,30 +141,32 @@ Each time the player guesses a letter, this function will check whether the game
   show the overlay screen with the “win” class and appropriate text. Otherwise, if the number of misses
    is equal to or greater than 5, show the overlay screen with the “lose” class and appropriate text. */
 
-qwerty(addEventListener)('click', e => {
+qwerty.addEventListener('button', e => {
+
+
     let letter = document.getElementsByClassName('letter'); //letter class added in addPhraseToDisplay(arr) function (moved from checkLetter function)
     //when letter chosen add 'chosen' class to that button
     //button elements with 'chosen' class should be disables and set to true
     let letterFound = document.getElementsByClassName('chosen'); //pass to checkLetter function
         function checkLetter(btnClicked) {
             
-            for (i=0; i>1; i++){} //loop through letter variable to see if match
-            //'if' match, add the 'show' class to list item containing that letter
-            //store the matching letter inside a variable, and return that letter
-            //'else' function returns null
+//             for (i=0; i>1; i++){} //loop through letter variable to see if match
+//             //'if' match, add the 'show' class to list item containing that letter
+//             //store the matching letter inside a variable, and return that letter
+//             //'else' function returns null
             
-            //Return function
-        };
-        if (letterFound === null)   {
-            //remove one of the tries from the scoreboard
-            //make sure to increase the missed count by 1
-            //change a liveHeart.png to a lostHeartpng image
-        } 
-        const checkWin = () => {
-            //each (for each statement?) time the player guesses letter check whether game won or lost
-            //check 'if' the number of letters with class 'show' is equal to the number of letters with class 'letters'
-            //'if' equal, show overlay screen with the class 'win'
-            // 'else if' ('number of misses >= 5) show the overlay with the screen with the class 'lose'
+//             //Return function
+//         };
+//         if (letterFound === null)   {
+//             //remove one of the tries from the scoreboard
+//             //make sure to increase the missed count by 1
+//             //change a liveHeart.png to a lostHeartpng image
+//         } 
+//         const checkWin = () => {
+//             //each (for each statement?) time the player guesses letter check whether game won or lost
+//             //check 'if' the number of letters with class 'show' is equal to the number of letters with class 'letters'
+//             //'if' equal, show overlay screen with the class 'win'
+//             // 'else if' ('number of misses >= 5) show the overlay with the screen with the class 'lose'
 
         };
 
