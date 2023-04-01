64b519e2f6a544cb608544fa127c42134fe00275
Hannah Cherba <hannahpond@yahoo.com>
2023-03-31 17:19:13 -0700

    worked out kinks in program. Cleaned up code.

--- game_show_app_v1.2/app.js
+++ game_show_app_v1.2/app.js
@@ -8,6 +8,19 @@ let startButton = document.getElementById('start-btn');
 
 let overlay = document.getElementById('overlay');
 
+let ul = document.getElementById('phrase');
+
+let h3Win = document.getElementById('h3-win');
+
+let h3Lose = document.getElementById('h3-lose'); 
+
+let heartImg = document.querySelectorAll("img");
+
+let buttons = document.querySelectorAll('button');
+
+let resetButton= document.querySelectorAll('.btn__reset'); 
+
+
 const phrases= [
     "total depravity",
     "unconditional election",
@@ -27,12 +40,6 @@ startButton.addEventListener('click', e => {
     }
 });
 
-/*Create a getRandomPhraseAsArray function.
-This function should randomly choose a phrase from the phrases array and split that phrase into a new array of characters. The function should 
-then return the new character array. Keep in mind that you’ll need to write this function so that it is reusable-- meaning that it can take any
- given array of strings (with no special characters) and return an array of characters. To do that, you’ll write 
- the function so that it takes an array as a parameter. */
-
  function getRandomPhraseAsArray(arr) {
 
     let phrase = arr[Math.floor(Math.random() * arr.length)];
@@ -40,22 +47,11 @@ then return the new character array. Keep in mind that you’ll need to write th
     return chars;
  };
 
- /* Set the game display.
- =========================
-Create an addPhraseToDisplay function that loops through an array of characters. Inside the loop, 
-for each character in the array, you’ll create a list item, put the character inside of the list 
-item, and append that list item to the #phrase ul in your HTML. If the character in the array is 
-a letter and not a space, the function should add the class “letter” to the list item.
-You’ll need to write the addPhraseToDisplay function so that it can take any array of letters 
-and add it to the display. To do that, the function will need to take an array as a parameter */
-
 const phraseArray = getRandomPhraseAsArray(phrases);
-let ul = document.getElementById('phrase');
- 
 
 const addPhraseToDisplay = (arr) => {
  
-    phraseArray.forEach((item)=>{
+    arr.forEach((item)=>{
 
         let li = document.createElement("li");
         li.textContent= item;
@@ -69,53 +65,9 @@ const addPhraseToDisplay = (arr) => {
         }
 
     })
-
 };
 
 addPhraseToDisplay(phraseArray); 
-
-/*Add an event listener to the keyboard.
-========================================
-Use event delegation to listen only to button events from the keyboard. When a player chooses a letter, 
-add the “chosen” class to that button so the same letter can’t be chosen twice. Note that button elements
- have an attribute you can set called “disabled” that when set to true will not respond to user clicks. 
- See the MDN documentation for more details.
-Pass the button to the checkLetter function, and store the letter returned inside of a variable called 
-letterFound. At this point, you can open the index.html file, click any of the letters on the keyboard, 
-and start to see the letters appear in the phrase. */
-
-/* Create a checkLetter function inside the keyboard Event Listener.
-==================================
-The checkLetter function will be used inside of the event listener you’ll write in the next step.
-This function should have one parameter: the button the player has clicked when guessing a letter.
-The checkLetter function should get all of the
-elements with a class of “letter” (remember that we added the letter class to all of the letters 
-    and none of the spaces when we made the game display). The function should loop over the letters
-     and check if they match the letter in the button the player has chosen.
-If there’s a match, the function should add the “show” class to the list item containing that letter,
- store the matching letter inside of a variable, and return that letter.
-If a match wasn’t found, the function should return null. */
-
-/*Count the missed guesses in the game.
-========================================
-If the checkLetter function returns a null value, the player has guessed the wrong letter. 
-In the keyboard event listener, after checkLetter is called, write a statement to check the 
-value of the letterFound variable. If the value is null, remove one of the tries from the scoreboard. 
-If you haven't created it yet, make sure you have a missed variable to store the state of the scoreboard 
-(initialized to 0). When you remove a try from the scoreboard, make sure to increase the missed count by 1. 
-Then change a liveHeart.png image to a lostHeart.png image. */
-
-/*Create a checkWin function.
-=================================
-Each time the player guesses a letter, this function will check whether the game has been won or lost.
- At the very end of the keyboard event listener, you’ll run this function to check if the number of 
- letters with class “show” is equal to the number of letters with class “letters”. If they’re equal,
-  show the overlay screen with the “win” class and appropriate text. Otherwise, if the number of misses
-   is equal to or greater than 5, show the overlay screen with the “lose” class and appropriate text. */
-   
-let heartImg = document.querySelectorAll("img");
-let buttons = document.querySelectorAll('button');
-
 
 for (let i = 0; i < buttons.length; i++) {
 
@@ -137,7 +89,6 @@ for (let i = 0; i < buttons.length; i++) {
                     letterMatch= letter[i];
                     letterMatch.classList.add('show');
                 } 
-            
             };
 
             return letterMatch;
@@ -145,9 +96,6 @@ for (let i = 0; i < buttons.length; i++) {
        
         const button= event.target;
         const letterFound = checkLetter(button);
-            
-        // let heartImg = document.querySelectorAll("img");
-            
 
         if (letterFound === null)   {
 
@@ -160,8 +108,6 @@ for (let i = 0; i < buttons.length; i++) {
 
             let lettersShown = document.getElementsByClassName('show');
             let startButtonText= document.getElementById('start-btn');
-            let h3Win = document.getElementById('h3-win');
-            let h3Lose = document.getElementById('h3-lose');
             
             
             if (lettersShown.length === letter.length) {
@@ -170,6 +116,8 @@ for (let i = 0; i < buttons.length; i++) {
                 overlay.style.display= 'flex';
                 h3Win.style.display= 'flex';
                 startButtonText.innerHTML="Play again!"
+                overlay.classList.remove('lose');
+                h3Lose.style.display= 'none';
                 
                 
             } else if (missed >= 5) {
@@ -178,19 +126,15 @@ for (let i = 0; i < buttons.length; i++) {
                 overlay.style.display= 'flex';
                 h3Lose.style.display= 'flex';
                 startButtonText.innerHTML="Try again!"
+                overlay.classList.remove('win');
+                h3Win.style.display= 'none';
         
             }
-
         }
         checkWin();
 
-    })
-    
+    })   
 }
-
-// Add a button to the “success” and “failure” screens that reset the game.
-//  You’ll have to recreate the buttons in the keyboard, generate a new random 
-//  phrase, and set the number of misses to zero.
 
 function resetGame() {
 
@@ -205,36 +149,31 @@ function resetGame() {
         }    
     }
     for (let i=0; i< buttons.length; i++) {
-
-        missed= 0;
-        ul.innerHTML=''; //called earlier outside my addPhraseToDisplay()
+       
+        ul.innerHTML=''; 
         buttons[i].disabled = false;
         buttons[i].classList.remove('chosen');
     } 
-
+    
     for (let i=0; i< letters.length; i++){
 
         letters[i].classList.remove('show');
+        
     }
 
-    phraseArray; 
-    addPhraseToDisplay(phraseArray); 
+    missed= 0;
+    const newPhrase = getRandomPhraseAsArray(phrases);
+    addPhraseToDisplay(newPhrase); 
 
 }
 
-let resetButton= document.querySelectorAll('.btn-reset'); 
+resetButton[0].addEventListener('click', e => {
 
-for (let i=0; i< resetButton.length; i++){
+    if (resetButton) {
 
-resetButton[i].addEventListener('click', e => {
+        resetGame()
 
-        if (resetButton) {
-
-            resetGame()
-
-            overlay.style.display= 'none';
-            
-        }
-
-    })
-}
+        overlay.style.display= 'none';
+        
+    }
+})
